a9bf16204a725a553a925490bb90c1fdecc0a93f
Klutrem <danyaryabov@mail.ru>
2023-06-29 00:16:30 +0300

    changed gin logs

--- lib/request_handler.go
+++ lib/request_handler.go
@@ -2,6 +2,8 @@ package lib
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // RequestHandler function
@@ -9,8 +11,28 @@ type RequestHandler struct {
 	Gin *gin.Engine
 }
 
+type logger interface {
+}
+
+type GinLogger struct {
+	*zap.SugaredLogger
+}
+
+func (l GinLogger) Write(p []byte) (n int, err error) {
+	l.Info(string(p))
+	return len(p), nil
+}
+
 // NewRequestHandler creates a new request handler
 func NewRequestHandler() RequestHandler {
+	config := zap.NewDevelopmentConfig()
+	level := zapcore.DebugLevel
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	config.Level.SetLevel(level)
+	zapLogger, _ := config.Build()
+	log := zapLogger.Sugar().WithOptions(zap.WithCaller(false))
+	logger := GinLogger{SugaredLogger: log}
+	gin.DefaultWriter = logger
 	engine := gin.New()
 	return RequestHandler{Gin: engine}
 }
